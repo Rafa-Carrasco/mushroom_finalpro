b3ddda482e50ab0d22614bf517ee71044785f045
Rafa-Carrasco <155183918+Rafa-Carrasco@users.noreply.github.com>
2024-07-09 14:19:25 +0000

    nueve

--- src/app copy.py
+++ src/app copy.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from pickle import load
 import json
 from sklearn.preprocessing import LabelEncoder
@@ -35,8 +37,7 @@ with open('label_encoders.json', 'r') as file:
 label_encoders = {}
 for column, mapping in label_mappings.items():
     encoder = LabelEncoder()
-    encoder.classes_ = np.array(mapping['classes'])
-    # encoder.classes_ = mapping['classes']
+    encoder.classes_ = mapping['classes']
     label_encoders[column] = encoder
 
 
@@ -66,24 +67,22 @@ veil_color = st.sidebar.selectbox("Color del velo", get_unique_values("veil-colo
 spore_print_color = st.sidebar.selectbox("Color de las esporas", get_unique_values("spore-print-color"))
 season = st.sidebar.selectbox("Temporada", get_unique_values("season"))
 
-# Transformar los valores seleccionados usando los LabelEncoders
-cap_shape_enc = label_encoders['cap-shape'].transform([cap_shape])[0]
-gill_color_enc = label_encoders['gill-color'].transform([gill_color])[0]
-stem_surface_enc = label_encoders['stem-surface'].transform([stem_surface])[0]
-stem_color_enc = label_encoders['stem-color'].transform([stem_color])[0]
-veil_color_enc = label_encoders['veil-color'].transform([veil_color])[0]
-spore_print_color_enc = label_encoders['spore-print-color'].transform([spore_print_color])[0]
-season_enc = label_encoders['season'].transform([season])[0]
 
 # --------------- MAIN BODY --------------------------------------
 
-st.title("Clasificador de setas")
+st.title("Clasificador de setas según su comestibilidad")
 st.text('Intro sobre los datos : problemas iniciales')
 # st.image('/images/seta1.jpg')
 if st.button("Predecir"):
-  
-    # prediction = str(model.predict([[cap_d, stem_h, stem_w, cap_shape, gill_color, stem_surface, stem_color, veil_color, spore_print_color, season ]])[0])  
-    prediction = str(model.predict([[cap_d, stem_h, stem_w, cap_shape_enc, gill_color_enc, stem_surface_enc, stem_color_enc, veil_color_enc, spore_print_color_enc, season_enc ]])[0])
+    cap_shape_enc = label_encoders['cap-shape'].transform([cap_shape])[0]
+    gill_color_enc = label_encoders['gill-color'].transform([gill_color])[0]
+    stem_surface_enc = label_encoders['stem-surface'].transform([stem_surface])[0]
+    stem_color_enc = label_encoders['stem-color'].transform([stem_color])[0]
+    veil_color_enc = label_encoders['veil-color'].transform([veil_color])[0]
+    spore_print_color_enc = label_encoders['spore-print-color'].transform([spore_print_color])[0]
+    season_enc = label_encoders['season'].transform([season])[0]
+      
+    prediction = str(model.predict([[cap_d, stem_h, stem_w, cap_shape, gill_color, stem_surface, stem_color, veil_color, spore_print_color, season ]])[0])
     pred_class = class_dict[prediction]
     st.write("Predicción:", pred_class)
     st.success('AVISO: Aunque este modelo tiene un 98.7 % de acierto en sus predicciones, no es recomendable comer setas silvestres sin autentico conocimiento del medio ')
